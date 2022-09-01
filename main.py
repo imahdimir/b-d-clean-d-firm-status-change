19889fdbac6bd97aa29998fd2707a045922cf71f
imahdimir <imahdimir@gmail.com>
2022-09-01 13:44:47 +0430

    v1

--- main.py
+++ main.py
@@ -1,57 +1,89 @@
 """
 
   """
+
 ##
 
 
-from datetime import time
-
 import pandas as pd
 from githubdata import GithubData
+from githubdata.main import _clean_github_url as cln_github_url
+from mirutil import utils as mu
 from mirutil.df_utils import read_data_according_to_type as read_data
+from mirutil.df_utils import save_as_prq_wo_index as sprq
 from persiantools.jdatetime import JalaliDate
 
 
-stch_url = 'imahdimir/d-status-changes'
+class ReposAddresses :
+  stch = 'imahdimir/d-status-changes'
+  targ = 'imahdimir/d-clean-d-firm-status-change'
 
-cjdt = 'JDateTime'
-cjd = 'JDate'
-cd = 'Date'
-row = 'Row'
-cdt = 'DateTime'
-cid = 'TSETMC_ID'
-cnews = 'NewStatus'
-ciso = 'iso'
-ctime = 'Time'
+ra = ReposAddresses()
 
-market_start_time = time(9 , 0 , 0)
-market_end_time = time(12 , 30 , 0)
+class ColNames :
+  id = 'TSETMC_ID'
+  jdt = 'JDateTime'
+  d = 'Date'
+  jd = 'JDate'
+  ns = 'NewStatus'
+  iso = 'iso'
+  dt = 'DateTime'
+  t = 'Time'
+  r = 'Row'
+
+c = ColNames()
 
 def main() :
   pass
   ##
-  rp_stch = GithubData(stch_url)
+  rp_stch = GithubData(ra.stch)
   ##
   rp_stch.clone()
   ##
   dsfp = rp_stch.data_fp
   ds = read_data(dsfp)
   ##
-  ds = ds.sort_values([cid , cjdt , row] , ascending = [False , False , True])
+  ds = ds.sort_values([c.id , c.jdt , c.r] , ascending = [False , False , True])
   ##
-  ds = ds.drop_duplicates([cid , cjdt] , keep = 'first')
+  msk = ds.duplicated([c.id , c.jdt] , keep = False)
+  df1 = ds[msk]
+  len(df1)
   ##
-  ds[cjd] = ds[cjdt].str[:10]
+  ds = ds.drop_duplicates([c.id , c.jdt] , keep = 'first')
   ##
-  ds[cjd] = ds[cjd].apply(lambda x : JalaliDate.fromisoformat(x))
+  ds[c.jd] = ds[c.jdt].str[:10]
   ##
-  ds[cd] = ds[cjd].apply(lambda x : x.to_gregorian())
+  ds[c.jd] = ds[c.jd].apply(lambda x : JalaliDate.fromisoformat(x))
   ##
-  ds[ciso] = ds[cd].astype(str) + ds[cjdt].str[10 :]
+  ds[c.d] = ds[c.jd].apply(lambda x : x.to_gregorian())
   ##
-  ds[cdt] = pd.to_datetime(ds['iso'] , format = '%Y-%m-%dT%H:%M:%S')
+  ds[c.iso] = ds[c.d].astype(str) + ds[c.jdt].str[10 :]
   ##
-  ds = ds[[cid , cdt , cnews]]
+  ds[c.dt] = pd.to_datetime(ds['iso'] , format = '%Y-%m-%dT%H:%M:%S')
+  ##
+  ds = ds[[c.id , c.jdt , c.dt , c.ns]]
+  ##
+  rp_targ = GithubData(ra.targ)
+  rp_targ.clone()
+  ##
+  dsfp = rp_targ.data_fp
+  ##
+  sprq(ds , dsfp)
+  ##
+  cur_url = cln_github_url(rp_targ.user_name + '/b-' + rp_targ.repo_name)
+  ##
+  msg = 'builded by:'
+  msg += ' ' + cur_url
+  ##
+  tokp = '/Users/mahdi/Dropbox/tok.txt'
+  tok = mu.get_tok_if_accessible(tokp)
+  ##
+  rp_targ.commit_and_push(msg , user = rp_targ.user_name , token = tok)
+
+  ##
+
+  rp_stch.rmdir()
+  rp_targ.rmdir()
 
   ##
 
